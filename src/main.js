a0a2a873f6da56add3f48e8c2754ad909d93de46
Junyoung Clare Jang <jjc9310@gmail.com>
2018-04-15 14:56:08 -0400

    Remove rxjs deps

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import $ from 'jquery';
-import Rx from 'rxjs';
 import * as THREE from 'three';
 import * as threePotree from '@pix4d/three-potree-loader';
 
@@ -18,7 +17,6 @@ $(() => {
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(0, 0);
 
-  const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(90, NaN, 0.001, 100000);
   camera.position.x = 292;
   camera.position.y = 143;
@@ -26,43 +24,51 @@ $(() => {
 
   const controls = new OrbitControls(camera, el);
 
-  const potree = new threePotree.Potree();
+  updateSize(renderer, camera, el);
+  window.addEventListener('resize', () => {
+    updateSize(renderer, camera, el);
+  });
 
-  updateSize();
   el.appendChild(renderer.domElement);
 
-  const pocObs = Rx.Observable.from(potree.loadPointCloud(serverConfig.cloudjs, serverConfig.makeURL))
-        .map((pco) => {
-          pco.material = new threePotree.PointCloudMaterial({
-            size: 3,
-            maxSize: 20,
-            minSize: 3,
-          });
-          pco.toTreeNode(pco.root);
-          scene.add(pco);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  const potree = new threePotree.Potree();
 
-  let prevNums = 0;
+  const scene = new THREE.Scene();
 
-  const renderObs = Rx.Observable.interval(0, Rx.Scheduler.animationFrame)
-        .map(() => {
-          renderer.render(scene, camera);
-        });
+  potree.loadPointCloud(serverConfig.cloudjs, serverConfig.makeURL)
+    .then((pco) => {
+      pco.material = new threePotree.PointCloudMaterial({
+        size: 3,
+        maxSize: 20,
+        minSize: 3,
+      });
+      pco.toTreeNode(pco.root);
 
-  const resizeObs = Rx.Observable.fromEvent(window, 'resize')
-        .map(updateSize);
+      return pco;
+    })
+    .then((pco) => {
+      scene.add(pco);
 
-  Rx.Observable
-    .merge(pocObs, renderObs, resizeObs)
-    .subscribe();
-
-  function updateSize() {
-    const { width, height } = el.getBoundingClientRect();
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-    renderer.setSize(width, height);
-  }
+      render(renderer, scene, camera);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
+
+function render(renderer, scene, camera, potree, pco) {
+  requestAnimationFrame(_render);
+
+  function _render() {
+    requestAnimationFrame(_render);
+
+    renderer.render(scene, camera);
+  }
+}
+
+function updateSize(renderer, camera, el) {
+  const { width, height } = el.getBoundingClientRect();
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+}
